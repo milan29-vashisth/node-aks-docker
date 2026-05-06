c2d73523a6ad6a9785995aaa7c92c5b5b1d48263
Milan <deepanshu.v7@gmail.com>
2026-05-06 13:02:57 +0530

    Update dcokerfile

---
+++ infra/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tf-backend-rg"
+    storage_account_name = "tfstatestorage292929"
+    container_name       = "tfstate"
+    key                  = "acr-terraform.tfstate"
+  }
+}
