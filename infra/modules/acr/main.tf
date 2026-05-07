1860f2cc95ba85ce17ae051a55b86f30fb7d1c1d
Milan <deepanshu.v7@gmail.com>
2026-05-07 09:15:35 +0530

    Update

--- infra/modules/acr/main.tf
+++ infra/modules/acr/main.tf
@@ -1,5 +1,11 @@
+resource "random_string" "acr_suffix" {
+  length  = 5
+  upper   = false
+  special = false
+}
+
 resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
+  name                = "nodeacr${random_string.acr_suffix.result}"
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = "Basic"
