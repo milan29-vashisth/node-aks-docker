ade65421c4d097b248e907fbf0ced8ddbe412b27
Milan <deepanshu.v7@gmail.com>
2026-05-07 15:39:30 +0530

    Update folers

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,5 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-  lifecycle {
-    prevent_destroy = true
-  }
+data "azurerm_resource_group" "rg" {
+  name = "tfstate-milan-rg"
 }
 
 module "acr" {
